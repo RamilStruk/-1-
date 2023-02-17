1a5141571fa0495c69c0f37c2282d1d712e92966
Рамиль Стрюк <124810588+RamilStruk@users.noreply.github.com>
2023-02-17 21:22:41 +0800

    4

---
+++ задача 8/Program.cs
@@ -0,0 +1,15 @@
+﻿// задача 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int x = 2;
+
+if(number > 1)
+{
+    while(x <= number)
+    {
+        Console.Write(x + " ");
+        x = x + 2;
+    }
+}
